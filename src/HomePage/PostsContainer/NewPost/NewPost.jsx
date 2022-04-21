7c1dd96a482625d3c18ea60457495488817a15bc
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-04-21 13:08:04 -0500

    fixed import

--- src/HomePage/PostsContainer/NewPost/NewPost.jsx
+++ src/HomePage/PostsContainer/NewPost/NewPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import Button from "@mui/material";
 
 class NewPost extends React.Component {
     constructor(props){
